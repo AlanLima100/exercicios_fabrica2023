bd77f640de53e7ad9898136d1f821fdf269b8625
AlanLima100 <alanvictor100@hotmail.com>
2023-03-27 17:32:30 -0300

    Subindo exercicos funcao retorna maior numero, multiplo de um numero, area ao quadrado

---
+++ exercicio_fabrica.py
@@ -0,0 +1,15 @@
+lucro = 100
+custo = 300
+
+faturamento  = lucro - custo
+
+if faturamento > 5000:
+    print(f"Bom faturamento")
+
+elif faturamento == 700:
+    print(f"Faturamento é de {faturamento}")
+
+else:
+    print("Mal faturamento")
+print(faturamento)
+ 
