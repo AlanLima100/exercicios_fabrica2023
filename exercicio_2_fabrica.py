708d2430e34d58402ebbfd6c5b07ac988f6ed623
AlanLima100 <alanvictor100@hotmail.com>
2023-03-28 16:27:00 -0300

    atualizando exercios

---
+++ exercicio_2_fabrica.py
@@ -0,0 +1,86 @@
+# DESCUBRA O TYPO E EVOLUÇÃO DO POKEMON
+
+
+
+						"evolves_to": [],
+						"is_baby": false,
+						"species": {
+							"name": "venusaur",
+							"url": "https://pokeapi.co/api/v2/pokemon-species/3/"
+						}
+					}
+				],
+				"is_baby": false,
+				"species": {
+					"name": "ivysaur",
+					"url": "https://pokeapi.co/api/v2/pokemon-species/2/"
+				}
+			}
+		],
+		"is_baby": false,
+		"species": {
+			"name": "bulbasaur",
+			"url": "https://pokeapi.co/api/v2/pokemon-species/1/"
+		}
+	},
+	"id": 1
+}
+
+"""
+
+"""
+						"evolves_to": [],
+						"is_baby": false,
+						"species": {
+							"name": "charizard",
+							"url": "https://pokeapi.co/api/v2/pokemon-species/6/"
+						}
+					}
+				],
+				"is_baby": false,
+				"species": {
+					"name": "charmeleon",
+					"url": "https://pokeapi.co/api/v2/pokemon-species/5/"
+				}
+			}
+		],
+		"is_baby": false,
+		"species": {
+			"name": "charmander",
+			"url": "https://pokeapi.co/api/v2/pokemon-species/4/"
+		}
+	},
+	"id": 2
+}
+
+
+"""
+
+
+
+"""
+						"evolves_to": [],
+						"is_baby": false,
+						"species": {
+							"name": "blastoise",
+							"url": "https://pokeapi.co/api/v2/pokemon-species/9/"
+						}
+					}
+				],
+				"is_baby": false,
+				"species": {
+					"name": "wartortle",
+					"url": "https://pokeapi.co/api/v2/pokemon-species/8/"
+				}
+			}
+		],
+		"is_baby": false,
+		"species": {
+			"name": "squirtle",
+			"url": "https://pokeapi.co/api/v2/pokemon-species/7/"
+		}
+	},
+	"id": 3
+}
+
+
