6c058b978e90a7a5b453995dd812f1267a323267
AlanLima100 <alanvictor100@hotmail.com>
2023-03-28 16:36:34 -0300

    exercicios da aula 01

--- exercicio_fabrica.py
+++ exercicio_fabrica.py
@@ -1,15 +1,43 @@
-lucro = 100
-custo = 300
 
-faturamento  = lucro - custo
-
-if faturamento > 5000:
-    print(f"Bom faturamento")
-
-elif faturamento == 700:
-    print(f"Faturamento é de {faturamento}")
-
-else:
-    print("Mal faturamento")
-print(faturamento)
  
+def maximo(num, num2):
+  maior = 0
+  if num > num2:
+    maior = num
+  else:
+    maior = num2
+
+  return f' O maior número entre {num1} e {num2} é {maior}' 
+
+
+num1 = int(input("Digite um número: "))
+num2 = int(input('Digite o segundo numero:'))
+
+
+print(maximo(num1, num2))
+
+
+def multiplo(num1, num2):
+  if num1 % num2 == 0:
+    return f'{num1}  é multiplo de {num2}  = {True}'
+  else:
+    return f'{num1} não é multiplo {num2}'
+  
+num1 = int(input("Digite um número: "))
+num2 = int(input('Digite o segundo numero:'))
+
+print(multiplo(num1, num2))
+
+
+
+
+def área(larg, comp):
+    a = larg * comp
+    print(f"A área de um terreno {larg} x {comp} é de {a} m².")
+
+
+print("Controle de Terrenos")
+print("-=" * 30)
+l = float(input("Largura (m): "))
+c = float(input("COMPRIMENTO (m):"))
+área(l, c)
